1ef14e195cf4a1782be3f904fd18e08b7fbd0327
amoskeys <lxgdays@163.com>
2016-12-08 16:36:22 +0800

    add selef query

--- src/main/java/com/amos/SpringDataJpa/repository/UserRepository.java
+++ src/main/java/com/amos/SpringDataJpa/repository/UserRepository.java
@@ -1,9 +1,18 @@
 package com.amos.SpringDataJpa.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.amos.SpringDataJpa.domain.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+	/**
+	 * 
+	 */
+	@Query("from User u where u.age>:age")
+	public List<User> findUsersByAge(@Param("age")Integer age);
 }
